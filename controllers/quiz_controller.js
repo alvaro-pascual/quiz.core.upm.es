ed71ce3ce362fae74ac8a17178c2664d765ac84d
Alvaro Pascual <alvaro.pascual.legido@alumnos.upm.es>
2016-05-21 21:27:33 +0200

    Tema 25: Roles y permisos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,6 +16,22 @@ exports.load = function(req, res, next, quizId) {
 };
 
 
+// MW que permite acciones solamente si al usuario logeado es admin o es el autor del quiz.
+exports.ownershipRequired = function(req, res, next){
+
+    var isAdmin      = req.session.user.isAdmin;
+    var quizAuthorId = req.quiz.AuthorId;
+    var loggedUserId = req.session.user.id;
+
+    if (isAdmin || quizAuthorId === loggedUserId) {
+        next();
+    } else {
+      console.log('Operación prohibida: El usuario logeado no es el autor del quiz, ni un administrador.');
+      res.send(403);
+    }
+};
+
+
 // GET /quizzes
 exports.index = function(req, res, next) {
 	models.Quiz.findAll()
@@ -59,11 +75,15 @@ exports.new = function(req, res, next) {
 
 // POST /quizzes/create
 exports.create = function(req, res, next) {
+
+  var authorId = req.session.user && req.session.user.id || 0;
+
   var quiz = models.Quiz.build({ question: req.body.quiz.question, 
-  	                             answer:   req.body.quiz.answer} );
+  	                             answer:   req.body.quiz.answer,
+                                 AuthorId: authorId } );
 
   // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["question", "answer"]})
+  quiz.save({fields: ["question", "answer", "AuthorId"]})
   	.then(function(quiz) {
 		  req.flash('success', 'Quiz creado con éxito.');
     	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
